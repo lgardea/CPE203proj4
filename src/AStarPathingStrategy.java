501277ea936ddb89732c072e13bdb951dee9425d
lgardea <terrx.l.gardea@gmail.com>
2019-11-30 13:22:37 -0800

    backtracking works!

--- src/AStarPathingStrategy.java
+++ src/AStarPathingStrategy.java
@@ -9,22 +9,25 @@ class AStarPathingStrategy
         implements PathingStrategy
 {
     HashMap closed = new HashMap();
+    PriorityQueue<Point> open = new PriorityQueue(10, new Comparator<Point>() {
+        public int compare(Point o1, Point o2) {
+            return (int)( (manhattanDistance(begin, o1) + manhattanDistance(last, o1))
+                    - (manhattanDistance(begin, o2) + manhattanDistance(last, o2)) );
+        }
+    });
+    Point begin = null;
+    Point last = null;
 
     public List<Point> computePath(Point start, Point end,
                                    Predicate<Point> canPassThrough,
                                    BiPredicate<Point, Point> withinReach,
                                    Function<Point, Stream<Point>> potentialNeighbors)
     {
+        if (begin == null){begin = start;}
+        if (last == null){last = end;}
         List<Point> path = new LinkedList<Point>();
-        PriorityQueue open = new PriorityQueue(10, new Comparator<Point>() {
-            public int compare(Point o1, Point o2) {
-                return (int)(manhattanDistance(start, o1) + manhattanDistance(start, o2));
-            }
-        });
 
-
-        if (!open.contains(start)){
-        open.add(start);}
+        if (!open.contains(begin)){ open.add(begin);}
         Point current = start;
 
             List<Point> validAdj = potentialNeighbors.apply(current)
@@ -33,22 +36,21 @@ class AStarPathingStrategy
                     .limit(4)
                     .collect(Collectors.toList());
 
-            double f = manhattanDistance(current, end) + 2;
-            Point next = null;
+            double f = manhattanDistance(open.peek(), begin) + manhattanDistance(open.peek(), end);
             for (Point pt : validAdj) {
                 if (!open.contains(pt)) {
                     open.add(pt);
                 }
-                double g = manhattanDistance(start, current);
+                double g = manhattanDistance(begin, current) + 1;
                 double h = manhattanDistance(pt, end);
                 if (g + h < f) {
                     f = g + h;
-                    next = pt;
                 }
-                closed.put(pt, g + h);
-                open.remove(pt);
+
             }
-            current = next;
+            closed.put(current, manhattanDistance(begin, current) + manhattanDistance(current, end) + 1);
+            open.remove(current);
+            current = open.peek();
             if (current != null){
             path.add(current);}
         return path;
